2a4414c0a0269cf6014291e7d19347f067821b10
Harold Carter <hcarter1112@gmail.com>
2020-07-22 15:17:26 -0500

    IT WORKS!!!

--- Biblebooks/bible_Books_Game.py
+++ Biblebooks/bible_Books_Game.py
@@ -80,25 +80,34 @@ def main():
 
     keepGoing = True
     repeat = True
+    number_correct = 0
     while repeat:
         while keepGoing:
             print("\nHow many questions would you like? ")
             num_questions = int(input())
-            keepGoing = Game(num_questions, bible_Books)
+            keepGoing = Game(num_questions, bible_Books, number_correct)
 
 
-def Game(num_questions, bible_Books):  # this is where the questions will be generated
+def Game(
+    num_questions, bible_Books, number_correct
+):  # this is where the questions will be generated
 
     number_left = num_questions
-    number_correct = 0
+    q_exceptions = []
     for questions in list(range(num_questions)):
         random_bit = random.getrandbits(1)
         random_boolean = bool(random_bit)
+        book_exceptions = []
+        ans_exceptions = []
         if random_boolean == True:
             random_boolean = "before"
         else:
             random_boolean = "after"
-        random_book = random.randrange(2, 65, 1)
+        random_book = random.randrange(2, 66, 1)
+        while random_book in q_exceptions:
+            random_book = random.randrange(2, 66, 1)
+        if random_book not in q_exceptions:
+            q_exceptions.append(random_book)
         print(
             "What book of the Bible comes "
             + random_boolean
@@ -109,41 +118,48 @@ def Game(num_questions, bible_Books):  # this is where the questions will be gen
 
         mult_letters = [1, 2, 3, 4]
         mult_letters_index = 0
-        book_exceptions = []
         for items in mult_letters:
             before_random_book = random_book - 1
             after_random_book = random_book + 1
-            answer_randomizer = random.randrange(0, 4, 1)
-            random_ans = random.randrange(2, 65, 1)
+            answer_randomizer = random.randrange(101, 105, 1)
+            random_ans = random.randrange(2, 66, 1)
             while answer_randomizer in book_exceptions:
-                answer_randomizer = random.randrange(0, 4, 1)
+                answer_randomizer = random.randrange(101, 105, 1)
             if answer_randomizer not in book_exceptions:
                 book_exceptions.append(answer_randomizer)
 
-            if answer_randomizer == 1 and random_boolean == "after":
+            while random_ans in ans_exceptions:
+                random_ans = random.randrange(1, 66, 1)
+            if random_ans not in ans_exceptions:
+                ans_exceptions.append(random_ans)
+            ans_exceptions.append(random_book)
+
+            if answer_randomizer == 101 and random_boolean == "after":
                 correct_answer = mult_letters_index + 1
+                ans_exceptions.append(after_random_book)
                 print(
                     str(mult_letters[mult_letters_index])
                     + ". "
                     + str(bible_Books[after_random_book])
                 )
                 mult_letters_index += 1
-            elif answer_randomizer == 2 and random_boolean == "before":
+            elif answer_randomizer == 102 and random_boolean == "before":
                 correct_answer = mult_letters_index + 1
+                ans_exceptions.append(after_random_book)
                 print(
                     str(mult_letters[mult_letters_index])
                     + ". "
-                    + str(bible_Books[after_random_book])
+                    + str(bible_Books[before_random_book])
                 )
                 mult_letters_index += 1
             else:
-                incorrect_answer = mult_letters_index + 1
                 print(
                     str(mult_letters[mult_letters_index])
                     + ". "
                     + bible_Books[random_ans]
                 )
                 mult_letters_index += 1
+
         ans = int(input("\nYour answer? "))
         if ans == correct_answer:
             print("\nCorrect!")
@@ -152,27 +168,58 @@ def Game(num_questions, bible_Books):  # this is where the questions will be gen
         else:
             print("\nIncorrect :,(")
 
-        if number_left < 2:
+        if number_left == 2:
             number_left -= 1
             print("Only " + str(number_left) + " question left...")
             print("-" * 80)
 
-        elif number_left <= 3:
+        elif number_left == 4 or number_left == 3:
             number_left -= 1
             print(
                 "THE FINAL COUNTDOOOWWWN!! " + str(number_left) + " questions left..."
             )
             print("-" * 80)
 
+        elif number_left == 0:
+            break
+
         else:
             number_left -= 1
             print("You have " + str(number_left) + " left...")
             print("-" * 80)
-    return False
+    grade = number_correct / num_questions
+    grade *= 100
 
+    if grade == 100:
+        print(
+            "Congratulations bible scholar! You got an 'A+' at "
+            + str(int(grade))
+            + "%. WOOT!"
+        )
+    elif grade >= 90 and grade < 100:
+        print("You got an 'A' at " + str(int(grade)) + "%. Awesome work!")
+    elif grade >= 80 and grade < 90:
+        print("You got an 'B' at " + str(int(grade)) + "%. Good job!")
+    elif grade >= 70 and grade < 80:
+        print("You got an 'C' at " + str(int(grade)) + "%. Well... You passed =)!")
+    elif grade >= 60 and grade < 70:
+        print("You got an 'D' at " + str(int(grade)) + "%. Ouch!")
+    else:
+        print(
+            "You got an 'F' at "
+            + str(int(grade))
+            + "%. JW.ORG has a free copy of the bible for you to study from!"
+        )
 
-def scorecard():  # this will dispay after the game has completed and will give them a grade for their performance. This should also include the option to play again
-    pass
+    print("\nWould you like to play again?")
+    print("\n1. Yes", "\n2. No\n")
+
+    play_again = int(input())
+
+    if play_again == 1:
+        return True
+    if play_again == 2:
+        return False
 
 
 if __name__ == "__main__":
